2a6f0baf0ed73f73152622c4624204265ddb39c7
menglulu <411277409@qq.com>
2019-11-07 16:09:27 -0500

    data loading and preprocessing with example

---
+++ data_preprocess.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 13:29:42 2019
+
+@author: lulumeng
+"""
+
+import pandas as pd
+from sklearn import preprocessing
+
+def data_read(file_path):
+    data_raw = pd.read_csv(file_path,sep=r'\t',header=None)
+    x_raw =  data_raw.iloc[:,:-1]
+    y = data_raw.iloc[:,-1]
+    # to do one-hot encoding for string column
+    x = pd.get_dummies(x_raw)
+    
+    x = x.values
+    y = y.values
+    
+    return x, y
+
+def data_norm(data):
+    #normoalize with min max scaler
+    min_max_scaler = preprocessing.MinMaxScaler()
+    data_scaled = min_max_scaler.fit_transform(data)
+    return data_scaled
+
+if __name__ == "__main__":
+    #example of data loading
+
+    x1,y1 = data_read("project3_dataset1.txt")
+    x1_sc = data_norm(x1)
+    
+    x2,y2 = data_read("project3_dataset2.txt")
+    x2_sc = data_norm(x2)
